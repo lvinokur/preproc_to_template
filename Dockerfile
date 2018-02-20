d8a087ca11c3149e3999dab0c810d585d1d790fb
lvinokur <lea.vinokur@gmail.com>
2018-02-20 11:56:35 +1100

    bug fixes of All analysis levels, script now runs to template (almost) - plus added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,62 @@
+FROM ubuntu:14.04
+MAINTAINER Lea Vinokur <lea.vinokur@gmail.com>
+ARG DEBIAN_FRONTEND=noninteractive
+# Core system capabilities required
+RUN apt-get update && apt-get install -y git python libeigen3-dev zlib1g-dev wget bsdtar software-properties-common curl tar perl-modules
+RUN curl -sL https://deb.nodesource.com/setup_4.x | bash -
+RUN apt-get install -y nodejs
+
+# Now that we have software-properties-common, can use add-apt-repository to get to g++ version 5, which is required by JSON for Modern C++
+RUN add-apt-repository ppa:ubuntu-toolchain-r/test
+RUN apt-get update && apt-get install -y g++-5
+
+# Neurodebian
+RUN wget -O- http://neuro.debian.net/lists/trusty.au.full | tee /etc/apt/sources.list.d/neurodebian.sources.list
+RUN apt-key adv --recv-keys --keyserver hkp://pgp.mit.edu:80 0xA5D32F012649A5A9 && apt-get update
+
+# fsl
+RUN apt-get install -y ants
+RUN apt-get install -y fsl-5.0-core
+RUN apt-get install -y fsl-first-data
+RUN apt-get install -y fsl-mni152-templates
+
+
+#MRtrix3 setup
+ENV CXX=/usr/bin/g++-5
+RUN git clone https://github.com/MRtrix3/mrtrix3.git mrtrix3 && cd mrtrix3 && git checkout dev && python configure -nogui && NUMBER_OF_PROCESSORS=1 python build && git describe --tags > /mrtrix3_version
+
+#RUN echo $'FailOnWarn: 1\n' > /etc/mrtrix.conf
+# Environment variables setup
+ENV FSLDIR=/usr/share/fsl/5.0
+ENV FSL_DIR="${FSLDIR}"
+ENV FSLOUTPUTTYPE=NIFTI_GZ
+ENV FSLMULTIFILEQUIT=TRUE
+ENV LD_LIBRARY_PATH=/usr/lib/fsl/5.0
+ENV PATH=/opt/freesurfer/bin:/opt/freesurfer/mni/bin:/usr/lib/fsl/5.0:/usr/lib/ants:/mrtrix3/bin:/opt/eddy:$PATH
+ENV PYTHONPATH=/mrtrix3/lib
+
+ENV FSLDIR=/usr/share/fsl/5.0
+ENV FSLMULTIFILEQUIT=TRUE
+# Note: Would prefer NIFTI, but need to stick to compressed for now due to FSL Ubuntu not honoring this variable. May be able to revert once fsl.checkFirst() is merged in.
+ENV FSLOUTPUTTYPE=NIFTI_GZ
+ENV LD_LIBRARY_PATH=/usr/lib/fsl/5.0
+ENV PATH=/usr/lib/fsl/5.0:/usr/lib/ants:/mrtrix3/bin:$PATH
+ENV PYTHONPATH=/mrtrix3/lib
+
+
+
+
+
+
+
+RUN mkdir /hcp_input
+RUN mkdir /bids_output
+RUN mkdir /hsm
+RUN mkdir /scratch
+RUN mkdir /vlsci
+
+COPY coeff_SC72C_Skyra.grad /Pipelines/global/config/coeff_SC72C_Skyra.grad
+COPY HCP_preproc.py /code/run.py
+RUN chmod 775 /code/run.py
+
+ENTRYPOINT ["/code/run.py"]
--- preproc_to_template.py
+++ preproc_to_template.py
@@ -20,6 +20,9 @@ app.cmdline.add_argument('analysis_level', help='Level of the analysis that will
                                                    'Valid choices are: [' + ', '.join(analysis_level_choices) + ']. \nMultiple participant '
                                                    'level analyses can be run independently(in parallel) using the same output_dir.',
                                               choices = analysis_level_choices)
+file_structure_options = ['bids', 'hcp']
+
+app.cmdline.add_argument ('file_structure', help='The structure in which the initial files are organised - bids or hcp')
 
 app.cmdline.add_argument('--participant_label', help='The label(s) of the participant(s) that should be analyzed. The label '
                                                  'corresponds to sub-<participant_label> from the BIDS spec '
@@ -38,8 +41,16 @@ if app.args.participant_label:
     subjects_to_analyze = app.args.participant_label
 # for all subjects
 else:
-    subject_dirs = glob.glob(os.path.join(app.args.in_dir, 'sub-*'))
-    subjects_to_analyze = [subject_dir.split("-")[-1] for subject_dir in subject_dirs]
+    if app.args.file_structure == 'bids':
+        subject_dirs = glob.glob(os.path.join(app.args.in_dir, 'sub-*'))
+        subjects_to_analyze = [subject_dir.split("-")[-1] for subject_dir in subject_dirs]
+
+    elif app.args.file_structure == 'hcp':
+        subject_dirs = os.listdir(app.args.in_dir)
+        subjects_to_analyze =  subject_dirs
+        print('Subject_dirs --- >    ', subject_dirs )
+    else:
+        app.error('No file structure specified for initial analysis')
 
 # create output subjects directory
 all_subjects_dir = os.path.join(app.args.output_dir, 'subjects');
@@ -58,48 +69,94 @@ app.makeTempDir()
 
 # loop over subjects and perform subject-level analysis (genereta mif, biascorrect, response functions)
 if app.args.analysis_level == 'participant1':
-    for subject_label in subjects_to_analyze:
-        label = 'sub-' + subject_label
-        print('running bias correction and rf estimation for ' + label)
+    if app.args.file_structure == 'bids':
+        for subject_label in subjects_to_analyze:
+            label = 'sub-' + subject_label
+            print('running bias correction and rf estimation for ' + label)
 
-        # Read DWI(s) in BIDS folder
-        all_dwi_images = glob.glob(os.path.join(app.args.in_dir, label, '*dwi', '*_dwi.nii*'))
+            # Read DWI(s) in BIDS folder
+            all_dwi_images = glob.glob(os.path.join(app.args.in_dir, label, '*dwi', '*_dwi.nii*'))
 
-        if (len(all_dwi_images) > 1):
-            app.error('Detecting more that one DWI - please fix')
+            if (len(all_dwi_images) > 1):
+                app.error('Detecting more that one DWI - please fix')
 
-        # Create output subject directory
-        subject_dir = os.path.join(all_subjects_dir, subject_label)
-        if not os.path.exists(subject_dir):
-            os.mkdir(subject_dir)
+            # Create output subject directory
+            subject_dir = os.path.join(all_subjects_dir, subject_label)
+            if not os.path.exists(subject_dir):
+                os.mkdir(subject_dir)
 
-        # Check existence output files from this analysis level
-        dwi_preproc_file = os.path.join(subject_dir, 'dwi_preproc.mif')
-        app.checkOutputPath(dwi_preproc_file)
-        wm_response_file = os.path.join(subject_dir, 'wm_response.txt')
-        app.checkOutputPath(wm_response_file)
-        gm_response_file = os.path.join(subject_dir, 'gm_response.txt')
-        app.checkOutputPath(gm_response_file)
-        csf_response_file = os.path.join(subject_dir, 'csf_response.txt')
-        app.checkOutputPath(csf_response_file)
+            # Check existence output files from this analysis level
+            dwi_preproc_file = os.path.join(subject_dir, 'dwi_preproc.mif')
+            app.checkOutputPath(dwi_preproc_file)
+            wm_response_file = os.path.join(subject_dir, 'wm_response.txt')
+            app.checkOutputPath(wm_response_file)
+            gm_response_file = os.path.join(subject_dir, 'gm_response.txt')
+            app.checkOutputPath(gm_response_file)
+            csf_response_file = os.path.join(subject_dir, 'csf_response.txt')
+            app.checkOutputPath(csf_response_file)
 
-        # DW gradient files
-        grad_prefix = os.path.join(app.args.in_dir, label, 'dwi', label + '_dwi')
-        if not (os.path.isfile(grad_prefix + '.bval') and os.path.isfile(grad_prefix + '.bvec')):
-            grad_prefix = os.path.join(app.args.in_dir, 'dwi')
+            # DW gradient files
+            grad_prefix = os.path.join(app.args.in_dir, label, 'dwi', label + '_dwi')
             if not (os.path.isfile(grad_prefix + '.bval') and os.path.isfile(grad_prefix + '.bvec')):
+                grad_prefix = os.path.join(app.args.in_dir, 'dwi')
+                if not (os.path.isfile(grad_prefix + '.bval') and os.path.isfile(grad_prefix + '.bvec')):
+                    app.error('Unable to locate valid diffusion gradient table');
+            grad_import_option = ' -fslgrad ' + grad_prefix + '.bvec ' + grad_prefix + '.bval'
+
+            # Stuff DWI gradients in *.mif file
+            dwi_mrtrix_file = os.path.join(app.tempDir, subject_label + '_dwi.mif')
+            run.command('mrconvert ' + all_dwi_images[0] + grad_import_option + ' ' + dwi_mrtrix_file + ' -force')
+
+            dwi_biascorrect_file = os.path.join(subject_dir, subject_label + '_dwi_biascorr.mif')
+            run.command('dwibiascorrect ' + dwi_mrtrix_file + ' ' + dwi_biascorrect_file + ' -ants ' + ' -force')
+
+            # Estimate WM, GM, CSF response functions
+            run.command('dwi2response ' + ' dhollander ' + dwi_biascorrect_file + ' ' + wm_response_file + ' ' + gm_response_file + ' ' + csf_response_file + ' -force')
+
+    elif app.args.file_structure == 'hcp':
+        for subject_label in subjects_to_analyze:
+            label = subject_label
+            print('running bias correction and rf estimation for ' + label)
+
+            # Read DWI(s) in HCP folder
+            dwi_hcp_folder = os.path.join(app.args.in_dir, label, 'T1w', 'Diffusion')
+            all_dwi_images = glob.glob(os.path.join(dwi_hcp_folder, '*data.nii*'))
+
+            if (len(all_dwi_images) > 1):
+                app.error('Detecting more that one DWI - please fix')
+
+            # Create output subject directory
+            subject_dir = os.path.join(all_subjects_dir, subject_label)
+            if not os.path.exists(subject_dir):
+                os.mkdir(subject_dir)
+
+            # Check existence output files from this analysis level
+            dwi_preproc_file = os.path.join(subject_dir, 'dwi_preproc.mif')
+            app.checkOutputPath(dwi_preproc_file)
+            wm_response_file = os.path.join(subject_dir, 'wm_response.txt')
+            app.checkOutputPath(wm_response_file)
+            gm_response_file = os.path.join(subject_dir, 'gm_response.txt')
+            app.checkOutputPath(gm_response_file)
+            csf_response_file = os.path.join(subject_dir, 'csf_response.txt')
+            app.checkOutputPath(csf_response_file)
+
+            # DW gradient files
+            bval_file = os.path.join(dwi_hcp_folder,'bvals')
+            bvec_file = os.path.join(dwi_hcp_folder,'bvecs')
+            if not (os.path.isfile(bval_file) and os.path.isfile(bvec_file)):
                 app.error('Unable to locate valid diffusion gradient table');
-        grad_import_option = ' -fslgrad ' + grad_prefix + '.bvec ' + grad_prefix + '.bval'
+            grad_import_option = ' -fslgrad ' + bvec_file + ' ' + bval_file
 
-        # Stuff DWI gradients in *.mif file
-        dwi_mrtrix_file = os.path.join(app.tempDir, subject_label + '_dwi.mif')
-        run.command('mrconvert ' + all_dwi_images[0] + grad_import_option + ' ' + dwi_mrtrix_file + ' -force')
+            # Stuff DWI gradients in *.mif file
+            dwi_mrtrix_file = os.path.join(app.tempDir, subject_label + '_dwi.mif')
+            run.command('mrconvert ' + all_dwi_images[0] + grad_import_option + ' ' + dwi_mrtrix_file + ' -force')
 
-        dwi_biascorrect_file = os.path.join(subject_dir, subject_label + '_dwi_biascorr.mif')
-        run.command('dwibiascorrect' + dwi_mrtrix_file + ' ' + dwi_biascorrect_file + ' -ants ' + ' -force')
+            dwi_biascorrect_file = os.path.join(subject_dir, subject_label + '_dwi_biascorr.mif')
+            run.command('dwibiascorrect ' + dwi_mrtrix_file + ' ' + dwi_biascorrect_file + ' -ants ' + ' -force')
 
-        # Estimate WM, GM, CSF response functions
-        run.command('dwi2response ' + ' dhollander ' + dwi_biascorrect_file + ' ' + wm_response_file + ' ' + gm_response_file + ' ' + csf_response_file + ' -force')
+            # Estimate WM, GM, CSF response functions
+            run.command('dwi2response ' + ' dhollander ' + dwi_biascorrect_file + ' ' + wm_response_file + ' ' + gm_response_file + ' ' + csf_response_file + ' -force')
+
 
 # running group level 1 (average response functions) TODO check for user supplied subset to ensure response is not biased
 elif app.args.analysis_level == "group1":
@@ -122,16 +179,16 @@ elif app.args.analysis_level == "group1":
     input_gm_files = glob.glob(os.path.join(all_subjects_dir, '*', 'gm_response.txt'))
     input_csf_files = glob.glob(os.path.join(all_subjects_dir, '*', 'csf_response.txt'))
 
-    run.command('average_response ' + ' '.join(input_wm_files) + ' ' + wm_response_file + app.force)
-    run.command('average_response ' + ' '.join(input_gm_files) + ' ' + gm_response_file + app.force)
-    run.command('average_response ' + ' '.join(input_csf_files) + ' ' + csf_response_file + app.force)
+    run.command('average_response ' + ' '.join(input_wm_files) + ' ' + wm_response_file )
+    run.command('average_response ' + ' '.join(input_gm_files) + ' ' + gm_response_file )
+    run.command('average_response ' + ' '.join(input_csf_files) + ' ' + csf_response_file )
 
 
 # running participant level 2 (upsample, compute brain masks and FODs, perform intensity normalisation and bias field correction)
 elif app.args.analysis_level == "participant2":
     print ('calculating masks and performing CSD')
     for subject_label in subjects_to_analyze:
-
+        print (' analysing subject -- > ', subject_label)
         subject_dir = os.path.join(all_subjects_dir, subject_label)
         output_mask = os.path.join(subject_dir, 'mask.mif')
         app.checkOutputPath(output_mask)
@@ -143,29 +200,31 @@ elif app.args.analysis_level == "participant2":
         app.checkOutputPath(output_csf)
 
         min_voxel_size = 1.25;
-        if app.args.vox_size:
-          min_voxel_size = float(app.args.vox_size)
 
-        voxel_sizes = getHeaderInfo(os.path.join(subject_dir, 'dwi_preproc.mif'), 'vox').split()
+
+        voxel_sizes = image.Header(os.path.join(subject_dir, subject_label + '_dwi_biascorr.mif')).spacing()
         mean_voxel_size = 0.0
         for i in range(0,3):
           mean_voxel_size = mean_voxel_size + float(voxel_sizes[i]) / 3.0
 
-        input_to_csd = os.path.join(subject_dir, 'dwi_preproc.mif')
-    # Compute brain mask
-    run.command('dwi2mask ' + input_to_csd + ' ' + output_mask + app.force)
+        input_to_csd = os.path.join(subject_dir, subject_label + '_dwi_biascorr.mif')
+        # Compute brain mask
+        print('dwi2mask ' + input_to_csd + ' ' + output_mask + ' -force')
+        run.command('dwi2mask ' + input_to_csd + ' ' + output_mask + ' -force')
 
-    #TODO add dilate mask by a voxel or two
 
-    # Perform CSD
-    run.command('dwi2fod msmt_csd ' + input_to_csd + ' -mask ' + output_mask + ' ' +
-                os.path.join(app.args.output_dir, 'average_wm_response.txt') + ' ' +  os.path.join(app.tempDir, subject_label + 'fod.mif') + ' ' +
-                os.path.join(app.args.output_dir, 'average_gm_response.txt') + ' ' + os.path.join(app.tempDir, subject_label + 'gm.mif') + ' ' +
-                os.path.join(app.args.output_dir, 'average_csf_response.txt') + ' ' + os.path.join(app.tempDir, subject_label + 'csf.mif'))
+        #TODO add dilate mask by a voxel or two
 
-    run.command('mtbin -independent ' + os.path.join(app.tempDir, subject_label + 'fod.mif') + ' ' + output_fod + ' ' +
-                                       os.path.join(app.tempDir, subject_label + 'gm.mif')  + ' ' + output_gm  + ' ' +
-                                       os.path.join(app.tempDir, subject_label + 'csf.mif') + ' ' + output_csf + app.force)
+        # Perform CSD
+        run.command('dwi2fod msmt_csd ' + input_to_csd + ' -mask ' + output_mask + ' ' +
+                    os.path.join(app.args.output_dir, 'average_wm_response.txt') + ' ' +  os.path.join(app.tempDir, subject_label + 'fod.mif') + ' ' +
+                    os.path.join(app.args.output_dir, 'average_gm_response.txt') + ' ' + os.path.join(app.tempDir, subject_label + 'gm.mif') + ' ' +
+                    os.path.join(app.args.output_dir, 'average_csf_response.txt') + ' ' + os.path.join(app.tempDir, subject_label + 'csf.mif'))
+
+        run.command('mtnormalise -mask ' + output_mask + ' -check_norm ' +  os.path.join(subject_dir, 'chk_norm.mif') + ' ' +
+                                           os.path.join(app.tempDir, subject_label + 'fod.mif') + ' ' + output_fod + ' ' +
+                                           os.path.join(app.tempDir, subject_label + 'gm.mif')  + ' ' + output_gm  + ' ' +
+                                           os.path.join(app.tempDir, subject_label + 'csf.mif') + ' ' + output_csf + ' -force')
 
 
 # running group level 2 (generate FOD template)
@@ -179,40 +238,22 @@ elif app.args.analysis_level == 'group2':
     os.mkdir('fod_input')
     os.mkdir('mask_input')
 
-    # Check if all members of subset exist
-    if (len(subset) > 0):
-        print('Using a group subset to compute population template' + str(subset))
-        subject_labels = [os.path.basename(x) for x in glob.glob(os.path.join(all_subjects_dir, '*'))]
-    for subj in subset:
-        if subj not in subject_labels:
-            app.error('subject label (' + os.path.basename(subj) + ') supplied as part of -group_subset option does exist in subjects directory')
-            app.checkOutputPath(os.path.join(all_subjects_dir, subj, 'subject2template_warp.mif'))
-            app.checkOutputPath(os.path.join(all_subjects_dir, subj, 'template2subject_warp.mif'))
-
-
     # make symlinks to all population_template inputs in single directory
     for subj in glob.glob(os.path.join(all_subjects_dir, '*')):
-        if (len(subset) > 0):
-            if os.path.basename(subj) in subset:
-                os.symlink(os.path.join(subj, 'fod.mif'), os.path.join('fod_input', os.path.basename(subj) + '.mif'))
-                os.symlink(os.path.join(subj, 'mask.mif'), os.path.join('mask_input', os.path.basename(subj) + '.mif'))
-        else:
           os.symlink(os.path.join(subj, 'fod.mif'), os.path.join('fod_input', os.path.basename(subj) + '.mif'))
           os.symlink(os.path.join(subj, 'mask.mif'), os.path.join('mask_input', os.path.basename(subj) + '.mif'))
 
     # Compute FOD template
-    if (len(subset) > 0):
-        run.command('population_template fod_input -mask mask_input ' + os.path.join(app.tempDir, 'tmp.mif'))
-    # Set a field in the header of the template to mark it as being generated as a subset or not. This is used in the next step
-        run.command('mrconvert ' + os.path.join(app.tempDir, 'tmp.mif') + ' -header_set made_from_subset true ' + fod_template + app.force)
-    else:
-        run.command('population_template fod_input -mask mask_input ' + os.path.join(app.tempDir, 'tmp.mif') + ' -warp_dir ' +  os.path.join(app.tempDir, 'warps'))
-        run.command('mrconvert ' + os.path.join(app.tempDir, 'tmp.mif') + ' -header_set made_from_subset false ' + fod_template + app.force)
+    run.command('population_template fod_input -mask mask_input ' + os.path.join(app.tempDir, 'tmp.mif') + ' -warp_dir ' +  os.path.join(app.tempDir, 'warps'))
+    run.command('mrconvert ' + os.path.join(app.tempDir, 'tmp.mif') + ' -header_set made_from_subset false ' + fod_template + ' -force')
     # Save all warps since we don't need to generate them in the next step if all subjects were used to make the template
     for subj in [os.path.basename(x) for x in glob.glob(os.path.join(all_subjects_dir, '*'))]:
-        run.command('warpconvert -type warpfull2deformation -template ' + fod_template + ' '
-                              + os.path.join(app.tempDir, 'warps', subj + '.mif') + ' '
-                              + os.path.join(all_subjects_dir, subj, 'subject2template_warp.mif') + app.force)
-        run.command('warpconvert -type warpfull2deformation -from 2 -template ' + os.path.join(all_subjects_dir, subj, 'fod.mif') + ' '
-                              + os.path.join(app.tempDir, 'warps', subj + '.mif') + ' '
-                              + os.path.join(all_subjects_dir, subj, 'template2subject_warp.mif') + app.force)
+        run.command('warpconvert -template ' + fod_template + ' '
+                              + os.path.join(app.tempDir, 'warps', subj + '.mif') + ' ' + warpfull2deformation + ' ' +
+                              + os.path.join(all_subjects_dir, subj, 'subject2template_warp.mif') + ' -force')
+        run.command('warpconvert -from 2 -template ' + os.path.join(all_subjects_dir, subj, 'fod.mif') + ' '
+                              + os.path.join(app.tempDir, 'warps', subj + '.mif') + ' ' + warpfull2deformation + ' '
+                              + os.path.join(all_subjects_dir, subj, 'template2subject_warp.mif') + ' -force')
+
+else:
+    app.error('no valid analysis level specified')
